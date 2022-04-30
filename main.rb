7a04a9bc3d31a04c7d45f5d9a920528354b8c08e
Shisui <aca.jevdjevic@gmail.com>
2022-04-30 17:58:10 +0200

    add convert to number method

--- main.rb
+++ main.rb
@@ -1,11 +1,10 @@
-# Create an array for the alphabet
-alphabet_upper_case = (65..90).to_a
-alphabet_lower_case = (97..122).to_a
-
-
-# check if the arrays output the right letters
-# alphabet_upper_case.each {|num| puts num.chr }
-# alphabet_lower_case.each {|num| puts num.chr}
+# # Create an array for the alphabet
+# alphabet_upper_case = (65..90).to_a
+# alphabet_lower_case = (97..122).to_a
+ 
+# # check if the arrays output the right letters
+# # alphabet_upper_case.each {|num| puts num.chr }
+# # alphabet_lower_case.each {|num| puts num.chr}
 
 # create a method to handle wrap z to a & vice-versa lowercase and uppercase
 def wrap_z_to_a(num)
@@ -29,4 +28,18 @@ def ask_user_input
     string = gets.chomp
     return string
 end
+# Ask user for input, i should convert it to numbers, lowercase/uppercase
 
+def convert_to_number_input (input)
+    
+    to_convert_input = input
+    input = to_convert_input.bytes
+   
+    return input
+end
+
+
+ 
+ converted_in_number = convert_to_number_input(ask_user_input)
+puts converted_in_number 
+ 
