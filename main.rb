87d4975c891b3e07503bdb6e9d54d86b0eb24d25
Shisui <aca.jevdjevic@gmail.com>
2022-04-30 19:56:21 +0200

    Fix bug
    the letters wouldn't wrap because the value from wrap wasn't transfered

--- main.rb
+++ main.rb
@@ -1,15 +1,15 @@
 require 'pry-byebug'
 # create a method to handle wrap z to a & vice-versa lowercase and uppercase
 def wrap_z_to_a(num)
-    binding.pry
+    
     if num == 91 # check for uppercase wrap z to a 
         num = 65
     elsif num == 64
         num = 90
     end
-    if num == 123 # check for lowercase wrap z to a
+    if num > 122 # check for lowercase wrap z to a
         num = 97
-    elsif num == 96
+    elsif num < 97
         num = 122
     end
     return num
@@ -17,7 +17,7 @@ end
 
 # ask user to input his text
 def ask_user_input
-    binding.pry
+    
     print "What text you want me to cipher: "
     string = gets.chomp
     return string
@@ -33,7 +33,7 @@ def convert_to_number_input (input)
 end
 # ask user for key 
 def ask_key
-    binding.pry
+    
     print "Give me the key for cipher: "
     string = gets.chomp
     
@@ -42,20 +42,21 @@ end
 
 def key_minus_num (num, key)
     
-    binding.pry
+    
     
     key = key.to_i
     
     z = 0
-    num2 = 0
+   
     while key > z do   # will add key minus each number and use wrap z to a at each iteration (each time we do minus to a num)
         num -= 1
         # wrap z to a 
-        num2 = wrap_z_to_a(num)
+        num = wrap_z_to_a(num)
   
         z = z + 1
     end
-    return num2
+    
+    return num
 end
 # i have to create a method to use my inputs to be able to return an output.
 # the inputs are the string of letters and the key.
@@ -64,7 +65,7 @@ end
 
 # cipher key should only act on the characters, not on blank space, punctuation..
 def the_cipher (string)
-    binding.pry
+    
     key = ask_key
     num = 0
     x = string.length
@@ -86,7 +87,7 @@ def the_cipher (string)
     return letters
 end
 #  now i have the input converted to number, i have to create a variable to hold the key
-binding.pry
+
 converted_in_numbers = the_cipher(ask_user_input)
 puts converted_in_numbers
  
