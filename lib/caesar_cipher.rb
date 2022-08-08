3c60191a89d909080018faa9be3cc3369911f174
aleksandar-code <aca.jevdjevic@gmail.com>
2022-08-08 15:27:08 +0200

    Refactoring

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,7 +1,36 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 class Caesar
-  def initialize
-      
+  def cipher(string, shift)
+    string = string.split("")
+    new_string = []
+    string.each do |char|
+      if ("a".."z").to_a.include?(char.downcase)
+        char = shift(char, shift)
+      end
+      new_string << char
+    end
+    new_string.join()
+  end
+
+  def shift(char, shift)
+    result = nil
+    shift = shift % 26
+    alphabet = ("a".."z").to_a
+    if alphabet.include?(char.downcase) 
+      shift = alphabet.find_index(char.downcase) + shift
+          result = alphabet[shift] if shift < alphabet.length
+          while result.nil?
+            shift = shift % 26
+            result = alphabet[shift]
+          end
+      if char.upcase == char 
+        result.upcase
+      else
+        result
+      end
+    end
   end
 end
