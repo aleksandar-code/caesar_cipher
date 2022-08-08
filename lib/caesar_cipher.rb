c7980a6b4606f61a7c152a74b57977eda80fa1de
aleksandar-code <aca.jevdjevic@gmail.com>
2022-08-08 16:03:42 +0200

    Refactor caesar

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,36 +1,17 @@
 # frozen_string_literal: true
 
-require 'pry-byebug'
-
 class Caesar
   def cipher(string, shift)
     string = string.split("")
     new_string = []
+    alphabet = ("a".."z").to_a
     string.each do |char|
-      if ("a".."z").to_a.include?(char.downcase)
-        char = shift(char, shift)
+      if alphabet.include?(char.downcase)
+        idx = (alphabet.find_index(char.downcase) + shift) % 26
+        char = char == char.upcase ? alphabet[idx].upcase : alphabet[idx]
       end
       new_string << char
     end
     new_string.join()
-  end
-
-  def shift(char, shift)
-    result = nil
-    shift = shift % 26
-    alphabet = ("a".."z").to_a
-    if alphabet.include?(char.downcase) 
-      shift = alphabet.find_index(char.downcase) + shift
-          result = alphabet[shift] if shift < alphabet.length
-          while result.nil?
-            shift = shift % 26
-            result = alphabet[shift]
-          end
-      if char.upcase == char 
-        result.upcase
-      else
-        result
-      end
-    end
   end
 end
